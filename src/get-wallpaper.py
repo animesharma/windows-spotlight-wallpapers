e8d0819c70d080070ffb30dbc599c63df56e6652
Animesh Sharma <animesharma08+github@gmail.com>
2023-10-15 05:10:35 -0400

    Code Refactor

--- src/get-wallpaper.py
+++ src/get-wallpaper.py
@@ -1,45 +1,46 @@
 import os
 import shutil
+from PIL import Image
+from tqdm import tqdm
 
-from tqdm import tqdm  # Import tqdm for the progress bar
-from PIL import Image  # Import the Python Imaging Library (PIL)
-
-# Get the user's profile directory
-user_profile = os.path.expandvars(r"%userprofile%")
-SPOTLIGHT_DIR = os.path.join(user_profile, "AppData", "Local", "Packages", "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets")
+# Constants
+USER_PROFILE = os.path.expandvars(r"%userprofile%")
+SPOTLIGHT_DIR = os.path.join(USER_PROFILE, "AppData", "Local", "Packages", "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy", "LocalState", "Assets")
 WALLPAPER_DIR = r"D:\Wallpapers"
-MIN_WIDTH = 1280  # Minimum width in pixels
-MIN_HEIGHT = 720  # Minimum height in pixels
+MIN_WIDTH, MIN_HEIGHT = 1280, 720  # Minimum dimensions in pixels
 
-if not os.path.exists(WALLPAPER_DIR):
-    os.makedirs(WALLPAPER_DIR)
+# Create the wallpaper directory if it doesn't exist
+os.makedirs(WALLPAPER_DIR, exist_ok=True)
 
-# List all files in SPOTLIGHT_DIR
+# Get all files in SPOTLIGHT_DIR
 spotlight_files = os.listdir(SPOTLIGHT_DIR)
 
 # Initialize tqdm with the total number of files
 with tqdm(total=len(spotlight_files), desc="Copying files") as pbar:
     for filename in spotlight_files:
         src_file = os.path.join(SPOTLIGHT_DIR, filename)
-        dst_file = os.path.join(WALLPAPER_DIR, filename + ".jpg")
+        dst_file = os.path.join(WALLPAPER_DIR, f"{filename}.jpg")
 
-        # Get the base filename (without extension)
-        base_filename, _ = os.path.splitext(filename)
+        # Skip if the file already exists in the destination
+        if os.path.exists(dst_file):
+            pbar.update(1)
+            continue
 
-        # Check if the file already exists in the destination
-        if not os.path.exists(dst_file):
-            try:
-                # Open the image and get its dimensions
-                img = Image.open(src_file)
+        try:
+            # Open the image and get its dimensions
+            with Image.open(src_file) as img:
                 width, height = img.size
-                # Check if dimensions meet the minimum requirements
-                if width >= MIN_WIDTH and height >= MIN_HEIGHT:
-                    # Copy the file
-                    shutil.copy2(src=src_file, dst=dst_file)  
-                else:
-                    print(f"Skipping {filename} (dimensions too small)")
-            except Exception as e:
-                print(f"Error processing {filename}: {e}")
+
+            # Check if dimensions meet the minimum requirements
+            if width < MIN_WIDTH or height < MIN_HEIGHT:
+                print(f"Skipping {filename} (dimensions too small)")
+                pbar.update(1)
+                continue
+
+            # Copy the file
+            shutil.copy2(src=src_file, dst=dst_file)
+        except Exception as e:
+            print(f"Error processing {filename}: {e}")
 
         # Update the progress bar
         pbar.update(1)
