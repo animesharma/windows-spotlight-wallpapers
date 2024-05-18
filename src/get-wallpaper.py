4ae93928713c1fb328b548788c7aabef9a64a79c
Animesh Sharma <animesharma08+github@gmail.com>
2024-05-18 15:40:03 -0400

    Performance improvements

--- src/get-wallpaper.py
+++ src/get-wallpaper.py
@@ -3,14 +3,14 @@ import shutil
 import logging
 from PIL import Image
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
 # Constants
 USER_PROFILE = Path.home()
 SPOTLIGHT_DIR = USER_PROFILE / "AppData" / "Local" / "Packages" / "Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy" / "LocalState" / "Assets"
 WALLPAPER_DIR = Path("D:/Wallpapers")
-MIN_WIDTH, MIN_HEIGHT = 1280, 720  # Minimum dimensions in pixels
+MIN_WIDTH_PX, MIN_HEIGHT_PX = 1280, 720  # Minimum dimensions in pixels
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -25,7 +25,7 @@ def process_file(entry: os.DirEntry[str]) -> None:
     dst_file = WALLPAPER_DIR / f"{entry.name}.jpg"
 
     # Skip if the file already exists in the destination
-    if dst_file.exists():
+    if dst_file.is_file():
         return
 
     try:
@@ -34,11 +34,12 @@ def process_file(entry: os.DirEntry[str]) -> None:
             width, height = img.size
 
         # Check if dimensions meet the minimum requirements
-        if width > MIN_WIDTH and height > MIN_HEIGHT:
-            # Copy the file
-            shutil.copy2(src_file, dst_file)
-        else:
+        if width < MIN_WIDTH_PX or height < MIN_HEIGHT_PX:
             logging.warning(f"Skipping {entry.name} (dimensions too small)")
+            return
+
+        # Copy the file
+        shutil.copy2(src_file, dst_file)
     except Exception as e:
         logging.error(f"Error processing {entry.name}: {e}")
 
@@ -46,12 +47,13 @@ def main() -> None:
     """Main function to process all files."""
     setup_directory(WALLPAPER_DIR)
 
-    with os.scandir(SPOTLIGHT_DIR) as entries:
-        spotlight_files = list(entries)
+    spotlight_files = (entry for entry in os.scandir(SPOTLIGHT_DIR) if entry.is_file())
+    total_files: int = sum(1 for f in os.scandir(SPOTLIGHT_DIR) if f.is_file())
 
-        with ThreadPoolExecutor() as executor:
-            for _ in tqdm(executor.map(process_file, spotlight_files), total=len(spotlight_files), desc="Copying files"):
-                pass
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(process_file, entry): entry for entry in spotlight_files}
+        for _ in tqdm(as_completed(futures), total=total_files, desc="Copying files"):
+            pass
 
 if __name__ == "__main__":
     main()
